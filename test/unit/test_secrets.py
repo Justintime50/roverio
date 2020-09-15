a62c519bcaca5242897c7534f8e3bfa3429d9c21
Justintime50 <justinpaulhammond@gmail.com>
2020-09-15 14:01:51 -0600

    Fixing tests

--- test/unit/test_secrets.py
+++ test/unit/test_secrets.py
@@ -3,9 +3,9 @@ from gatekeeper import secrets
 
 def test_secrets_no_gitignore():
     result = secrets.Secrets.run('gatekeeper', 20)
-    assert 'Secret: file_extension_files' in result[0]
+    assert 'File: gatekeeper/file_extension.py\nSecret: file_extension_files\nLine: 43\n' in result  # noqa
 
 
 def test_secrets_gitignore():
     result = secrets.Secrets.run('./', 20)
-    assert 'Secret: long_description_content_type' in result[0]
+    assert 'File: ./setup.py\nSecret: long_description_content_type\nLine: 11\n' in result  # noqa
